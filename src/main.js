61da7bcdbc9dea3853581f747fa8f719a13bdbe9
Jayden He <jayden@local.com>
2024-03-16 13:14:53 +0800

    enable analysis

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import { basename, extname } from 'path'
 import { config } from '@xytoki/asset-loader'
+import { inject } from '@vercel/analytics';
 config.urlToKey = function(url) {
     return basename(url).replace(extname(url), '')
 }
+inject();
 createApp(App)
     .use(router)
     .mount('#toki')
